277111b709c6ecd1adcce4b00f7fd46cdf60ae61
Hetvi Trivedi <hetvit27@gmail.com>
2023-01-30 21:04:08 -0800

    save tutors

--- src/main/java/com/nighthawk/spring_portfolio/mvc/practiceProblems/PracticeInit.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/practiceProblems/PracticeInit.java
@@ -9,29 +9,30 @@ import org.springframework.stereotype.Component;
 
 @Component // Scans Application for ModelInit Bean, this detects CommandLineRunner
 public class PracticeInit {
-    
+
     // Inject repositories
-    @Autowired PracticeJpaRepository repository;
-    
+    @Autowired
+    PracticeJpaRepository repository;
+
     @Bean
-    CommandLineRunner runPractice() {  // The run() method will be executed after the application starts
+    CommandLineRunner runPractice() { // The run() method will be executed after the application starts
         return args -> {
             // Fail safe data validations
 
             // starting jokes
             final String[] problemsArray = {
-                "Two students, S1 and S2, are rushing to class. They are waiting for green light at the same intersection. As soon as the light turns green, both students start walking with constant acceleration. The acceleration of student S1 is a1, and the acceleration of student S2 is a2, When student S1 has reached a velocity v1, she has walked a distance D1, while student S2 has walked a distance D2 = 1/3D1. In terms of v1 and D1, what is the correct expression for a2, the acceleration of student S2?",
-                "Copenhagen is located 780 km North and 810 km East of Paris. A flight from Paris to Copenhagen takes two hours. Typically the wind blows from East to West over Europe, but the speed varies. For one particular flight, the wind speed was 165 km/h. What is the magnitude of vpa, the plane's velocity with respect to the air?",
+                    "Two students, S1 and S2, are rushing to class. They are waiting for green light at the same intersection. As soon as the light turns green, both students start walking with constant acceleration. The acceleration of student S1 is a1, and the acceleration of student S2 is a2, When student S1 has reached a velocity v1, she has walked a distance D1, while student S2 has walked a distance D2 = 1/3D1. In terms of v1 and D1, what is the correct expression for a2, the acceleration of student S2?",
+                    "Copenhagen is located 780 km North and 810 km East of Paris. A flight from Paris to Copenhagen takes two hours. Typically the wind blows from East to West over Europe, but the speed varies. For one particular flight, the wind speed was 165 km/h. What is the magnitude of vpa, the plane's velocity with respect to the air?",
             };
 
             // make sure Joke database is populated with starting jokes
             for (String problems : problemsArray) {
-                List<Practice> test = repository.findByProblemIgnoreCase(problems);  // JPA lookup
+                List<Practice> test = repository.findByProblemIgnoreCase(problems); // JPA lookup
                 if (test.size() == 0)
-                    repository.save(new Practice(null, problems, 1, "1.1 Newton's Laws", "physics, newton's laws, force, mass, acceleration")); //JPA save
+                    repository.save(new Practice(null, problems, 1, "1.1 Newton's Laws",
+                            "physics, newton's laws, force, mass, acceleration")); // JPA save
             }
-            
+
         };
     }
 }
-
--- src/main/java/com/nighthawk/spring_portfolio/mvc/tutors/TutorsApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/tutors/TutorsApiController.java
@@ -1,4 +1,4 @@
-package com.nighthawk.spring_portfolio.mvc.practiceProblems;
+package com.nighthawk.spring_portfolio.mvc.tutors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,13 +8,13 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController // annotation to simplify the creation of RESTful web services
-@RequestMapping("/api/problems") // all requests in file begin with this URI
-public class PracticeApiController {
+@RequestMapping("/api/tutors") // all requests in file begin with this URI
+public class TutorsApiController {
 
     // Autowired enables Control to connect URI request and POJO Object to easily
     // for Database CRUD operations
     @Autowired
-    private PracticeJpaRepository repository;
+    private TutorsJpaRepository repository;
 
     /*
      * GET List of Jokes
@@ -23,7 +23,7 @@ public class PracticeApiController {
      * handler methods.
      */
     @GetMapping("/")
-    public ResponseEntity<List<Practice>> getProblems() {
+    public ResponseEntity<List<Tutors>> getTutors() {
         // ResponseEntity returns List of Jokes provide by JPA findAll()
         return new ResponseEntity<>(repository.findAll(), HttpStatus.OK);
     }
@@ -37,19 +37,19 @@ public class PracticeApiController {
      * @PathVariable annotation extracts the templated part {id}, from the URI
      */
     @PutMapping("/like/{id}")
-    public ResponseEntity<Practice> setLike(@PathVariable long id) {
+    public ResponseEntity<Tutors> setLike(@PathVariable long id) {
         /*
          * Optional (below) is a container object which helps determine if a result is
          * present.
          * If a value is present, isPresent() will return true
          * get() will return the value.
          */
-        Optional<Practice> optional = repository.findById(id);
+        Optional<Tutors> optional = repository.findById(id);
         if (optional.isPresent()) { // Good ID
-            Practice problems = optional.get(); // value from findByID
-            problems.setUnit(problems.getUnit() + 1); // increment value
-            repository.save(problems); // save entity
-            return new ResponseEntity<>(problems, HttpStatus.OK); // OK HTTP response: status code, headers, and body
+            Tutors tutors = optional.get(); // value from findByID
+            tutors.setLocation(tutors.getLocation() + 1); // increment value
+            repository.save(tutors); // save entity
+            return new ResponseEntity<>(tutors, HttpStatus.OK); // OK HTTP response: status code, headers, and body
         }
         // Bad ID
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST); // Failed HTTP response: status code, headers, and body
@@ -59,26 +59,26 @@ public class PracticeApiController {
      * Update Jeer
      */
     @PutMapping("/jeer/{id}")
-    public ResponseEntity<Practice> setJeer(@PathVariable long id) {
-        Optional<Practice> optional = repository.findById(id);
+    public ResponseEntity<Tutors> setJeer(@PathVariable long id) {
+        Optional<Tutors> optional = repository.findById(id);
         if (optional.isPresent()) { // Good ID
-            Practice problems = optional.get();
-            problems.setTopic(problems.getTopic() + 1);
-            repository.save(problems);
-            return new ResponseEntity<>(problems, HttpStatus.OK);
+            Tutors tutors = optional.get();
+            tutors.setExperience(tutors.getExperience() + 1);
+            repository.save(tutors);
+            return new ResponseEntity<>(tutors, HttpStatus.OK);
         }
         // Bad ID
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
     @PutMapping("/share/{id}")
-    public ResponseEntity<Practice> setTags(@PathVariable long id) {
-        Optional<Practice> optional = repository.findById(id);
+    public ResponseEntity<Tutors> setTags(@PathVariable long id) {
+        Optional<Tutors> optional = repository.findById(id);
         if (optional.isPresent()) { // Good ID
-            Practice problems = optional.get();
-            problems.setTags(problems.getTags() + 1);
-            repository.save(problems);
-            return new ResponseEntity<>(problems, HttpStatus.OK);
+            Tutors tutors = optional.get();
+            tutors.setPastReviews(tutors.getPastReviews() + 1);
+            repository.save(tutors);
+            return new ResponseEntity<>(tutors, HttpStatus.OK);
         }
         // Bad ID
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
