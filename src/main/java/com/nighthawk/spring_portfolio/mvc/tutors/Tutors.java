ddc167f80d635fc626d345988aeb7622cc32ac04
Hetvi Trivedi <hetvit27@gmail.com>
2023-02-02 13:34:15 -0800

    push

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/tutors/Tutors.java
@@ -0,0 +1,25 @@
+package com.nighthawk.spring_portfolio.mvc.tutors;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data // Annotations to simplify writing code (ie constructors, setters)
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity // Annotation to simplify creating an entity, which is a lightweight persistence
+        // domain object. Typically, an entity represents a table in a relational
+        // database, and each entity instance corresponds to a row in that table.
+public class Tutors {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id; // Unique identifier
+
+    @Column(unique = true)
+    private String tutor; // The Joke
+
+    private int like; // Store joke likes
+    private int dislike; // Store joke jeers
+}
---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/tutors/TutorsApiController.java
@@ -0,0 +1,86 @@
+package com.nighthawk.spring_portfolio.mvc.tutors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController // annotation to simplify the creation of RESTful web services
+@RequestMapping("/api/tutors") // all requests in file begin with this URI
+public class TutorsApiController {
+
+    // Autowired enables Control to connect URI request and POJO Object to easily
+    // for Database CRUD operations
+    @Autowired
+    private TutorsJpaRepository repository;
+
+    /*
+     * GET List of Physics
+     * 
+     * @GetMapping annotation is used for mapping HTTP GET requests onto specific
+     * handler methods.
+     */
+    @GetMapping("/")
+    public ResponseEntity<List<Tutors>> getTutors() {
+        // ResponseEntity returns List of Physics provide by JPA findAll()
+        return new ResponseEntity<>(repository.findAll(), HttpStatus.OK);
+    }
+
+    /*
+     * Update Like
+     * 
+     * @PutMapping annotation is used for mapping HTTP PUT requests onto specific
+     * handler methods.
+     * 
+     * @PathVariable annotation extracts the templated part {id}, from the URI
+     */
+    @PutMapping("/like/{id}")
+    public ResponseEntity<Tutors> setLike(@PathVariable long id) {
+        /*
+         * Optional (below) is a container object which helps determine if a result is
+         * present.
+         * If a value is present, isPresent() will return true
+         * get() will return the value.
+         */
+        Optional<Tutors> optional = repository.findById(id);
+        if (optional.isPresent()) { // Good ID
+            Tutors tutor = optional.get(); // value from findByID
+            tutor.setLike(tutor.getLike() + 1); // increment value
+            repository.save(tutor); // save entity
+            return new ResponseEntity<>(tutor, HttpStatus.OK); // OK HTTP response: status code, headers, and body
+        }
+        // Bad ID
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST); // Failed HTTP response: status code, headers, and body
+    }
+
+    /*
+     * Update Dislike
+     */
+    @PutMapping("/dislike/{id}")
+    public ResponseEntity<Tutors> setDislike(@PathVariable long id) {
+        Optional<Tutors> optional = repository.findById(id);
+        if (optional.isPresent()) { // Good ID
+            Tutors tutor = optional.get();
+            tutor.setDislike(tutor.getDislike() + 1);
+            repository.save(tutor);
+            return new ResponseEntity<>(tutor, HttpStatus.OK);
+        }
+        // Bad ID
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    // @PutMapping("/share/{id}")
+    // public ResponseEntity<Physics> setShare(@PathVariable long id) {
+    // Optional<Physics> optional = repository.findById(id);
+    // if (optional.isPresent()) { // Good ID
+    // Physics equation = optional.get();
+    // equation.setShare(equation.getShare()+1);
+    // repository.save(equation);
+    // return new ResponseEntity<>(equation, HttpStatus.OK);
+    // }
+    // // Bad ID
+    // return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    // }
+}
