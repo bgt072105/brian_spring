5bb7084ea88fcc2c3b9b17652d7a83e4757180d1
xu-ellen <xu.ellen@outlook.com>
2023-02-10 10:46:04 -0800

    add equation

--- src/main/java/com/nighthawk/spring_portfolio/mvc/equations/EquationApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/equations/EquationApiController.java
@@ -6,6 +6,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.nighthawk.spring_portfolio.mvc.person.Person;
+import com.nighthawk.spring_portfolio.mvc.person.PersonJpaRepository;
+
 import java.util.*;
 import java.text.SimpleDateFormat;
 
@@ -21,6 +24,9 @@ public class EquationApiController {
     @Autowired
     private EquationJpaRepository repository;
 
+    @Autowired
+    private PersonJpaRepository personRepository;
+
     /*
      * GET List of Equations (for debugging, later on will block this)
      */
@@ -32,7 +38,15 @@ public class EquationApiController {
     }
 
     /* Post request to add a new equation */
-    
+    @PostMapping("/add")
+    public ResponseEntity<Equation> addEquation(@RequestParam("person_id") int person_id, @RequestParam("text") String text) {
+        Person person = personRepository.getPersonById(person_id);
+        Equation equation = new Equation(text, person);
+        repository.save(equation);
+        return new ResponseEntity<>(equation, HttpStatus.OK);  // OK HTTP response: status code, headers, and body
+    }
+
+
     /*
      * GET individual Person equations using ID
      */
