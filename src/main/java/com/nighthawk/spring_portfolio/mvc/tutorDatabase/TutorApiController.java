b810cfe6cc6aa4c2b7fb7779a588c964df71a4e9
Iris Yang <irisyangdoudou@gmail.com>
2023-02-09 22:54:52 -0800

    tutor

--- src/main/java/com/nighthawk/spring_portfolio/mvc/tutorDatabase/TutorApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/tutorDatabase/TutorApiController.java
@@ -1,4 +1,4 @@
-package com.nighthawk.spring_portfolio.mvc.discussions;
+package com.nighthawk.spring_portfolio.mvc.tutorDatabase;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,13 +9,13 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController // annotation to simplify the creation of RESTful web services
-@RequestMapping("/api/discussions") // all requests in file begin with this URI
-public class DiscussionApiController {
+@RequestMapping("/api/tutor") // all requests in file begin with this URI
+public class TutorApiController {
 
     // Autowired enables Control to connect URI request and POJO Object to easily
     // for Database CRUD operations
     @Autowired
-    private DiscussionJpaRepository repository;
+    private TutorJpaRepository repository;
 
     /*
      * GET List of Jokes
@@ -24,7 +24,7 @@ public class DiscussionApiController {
      * handler methods.
      */
     @GetMapping("/")
-    public ResponseEntity<List<Discussion>> getQuestion() {
+    public ResponseEntity<List<Tutor>> getQuestion() {
         // ResponseEntity returns List of Jokes provide by JPA findAll()
         return new ResponseEntity<>(repository.findAll(), HttpStatus.OK);
     }
@@ -38,15 +38,15 @@ public class DiscussionApiController {
      * @PathVariable annotation extracts the templated part {id}, from the URI
      */
     @PostMapping("/add")
-    public ResponseEntity<Discussion> addQuestion(@RequestParam("question") String question,
+    public ResponseEntity<Tutor> addQuestion(@RequestParam("question") String question,
             @RequestParam("Unit") int Unit,
             @RequestParam("Tags") String Tags,
             @RequestParam("Email") String Email) {
-        repository.save(new Discussion(null, question, Unit, Tags, Email)); // JPA save
+        repository.save(new Tutor(null, question, Unit, Tags, Email)); // JPA save
         long maxId = repository.getMaxId();
-        Optional<Discussion> optional = repository.findById(maxId);
+        Optional<Tutor> optional = repository.findById(maxId);
         if (optional.isPresent()) {
-            Discussion question1 = optional.get();
+            Tutor question1 = optional.get();
             return new ResponseEntity(question1, HttpStatus.OK);
         }
         // Bad ID
@@ -57,7 +57,7 @@ public class DiscussionApiController {
      * GET individual Person using ID
      */
     @GetMapping("/search")
-    public ResponseEntity<List<Discussion>> getPerson(@RequestParam("question") String term) {
+    public ResponseEntity<List<Tutor>> getPerson(@RequestParam("question") String term) {
         return new ResponseEntity<>(repository.findByQuestionorEmail(term), HttpStatus.OK);
     }
 
@@ -65,13 +65,13 @@ public class DiscussionApiController {
      * Update Jeer
      */
     @PostMapping("/jeer/{id}")
-    public ResponseEntity<Discussion> setJeer(@PathVariable long id) {
+    public ResponseEntity<Tutor> setJeer(@PathVariable long id) {
         // Bad ID
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
     @PostMapping("/share/{id}")
-    public ResponseEntity<Discussion> setTags(@PathVariable long id) {
+    public ResponseEntity<Tutor> setTags(@PathVariable long id) {
         // Bad ID
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
